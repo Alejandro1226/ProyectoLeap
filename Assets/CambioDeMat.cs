e8b0ab7d09ca2e551f2b25580296ff7556353c87
Alejandro1226 <alejandro.valencia_g@uao.edu.co>
2021-11-05 11:39:55 -0500

    Leap Terminado y script de colision
    Se termina de realizar la configuración del leap motion y queda funcionando. Se intenta crear un script para detectar colisiones entre el leap y un cubo para cambiar el material del cubo, dentro de la escena.

---
+++ Assets/CambioDeMat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioDeMat : MonoBehaviour
+{
+    public Material[] mat;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = mat[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Golpe")
+        {
+            rend.sharedMaterial = mat[1];
+        }
+        else
+        {
+            rend.sharedMaterial = mat[2];
+        }
+    }
+}
