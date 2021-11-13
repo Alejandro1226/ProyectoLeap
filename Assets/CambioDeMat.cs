be839b64900ebc8ca5ae4968e3dd336ef7acd00d
Alejandro1226 <alejandro.valencia_g@uao.edu.co>
2021-11-13 13:23:27 -0500

    edición de scripts
    Se intenta hacer un script de cambio de materiales, y se agrega a un tambor el script de frenar

--- Assets/CambioDeMat.cs
+++ Assets/CambioDeMat.cs
@@ -22,7 +22,7 @@ public class CambioDeMat : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Golpe")
         {
